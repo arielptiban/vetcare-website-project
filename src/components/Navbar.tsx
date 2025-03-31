840d0625cd9729f3ae0a48ef4a994f10660dabac
Ariel <user@MacBook-Pro-de-Ariel.local>
2025-03-31 08:50:26 -0500

    Update Navbar section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,14 +20,14 @@ export default function Navbar() {
         return () => window.removeEventListener('scroll', handleScroll)
     }, []);
 
-    //responsive menu
-    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-    const closeMenu = () => setIsMenuOpen(false);
-
 
     return (
         <>
-            <header className="fixed top-0 left-0 right-0 z-50 transition-all duration-300 bg-transparent py-4" >
+            <header
+                className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300  ${scrolled
+                    ? 'bg-white/90 backdrop-blur-md shadow-sm py-2'
+                    : 'bg-transparent py-4'
+                    }`}>
                 <div className="max-w-[1400px] mx-auto px-4 md:px-6 flex justify-between items-center">
                     <VitalCareLogo />
 
